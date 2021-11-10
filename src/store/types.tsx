ec60a48aecf6c2d6c8e18789401824534fab091c
Даша Бо <dasha_bo@MacBook-Air-Dasa.local>
2021-11-10 14:32:39 +0400

    Kill off any in TodoItemsAction

--- src/store/types.tsx
+++ src/store/types.tsx
@@ -1,11 +1,21 @@
 export interface TodoItem {
-    id: string;
-    title: string;
-    details?: string;
-    done: boolean;
-  }
-  
-  export interface TodoItemsState {
-    todoItems: TodoItem[];
-  }
-  
+  id: string;
+  title: string;
+  details?: string;
+  done: boolean;
+}
+
+export interface TodoItemsState {
+  todoItems: TodoItem[];
+}
+
+export interface ITodoItemsAction<T, P> {
+  type: T;
+  data: P;
+}
+
+export type TodoItemsAction =
+  | ITodoItemsAction<"loadState" | "reorder", { todoItems: TodoItem[] }>
+  | ITodoItemsAction<"add", { todoItem: { title: string; details?: string } }>
+  | ITodoItemsAction<"delete" | "toggleDone", { id: string }>
+  | ITodoItemsAction<"edit", TodoItem>;
